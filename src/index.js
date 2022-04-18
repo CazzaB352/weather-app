e222058cc53d557fd91f0b7467a4b43948510d0d
CazzaB352 <99604109+CazzaB352@users.noreply.github.com>
2022-04-18 15:24:13 +0100

    css and js coding so far copied over
    from code sandbox to VS editior

---
+++ src/index.js
@@ -0,0 +1,54 @@
+let now = new Date();
+let h1 = document.querySelector("h1");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "friday",
+  "Satureday",
+];
+
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+h1.innerHTML = `${day} ${hour}:${minutes}`;
+
+function showTemp(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#current-temp");
+  let description = document.querySelector("#temp-description");
+  temperatureElement.innerHTML = `${temperature}°C`;
+  description.innerHTML = response.data.weather[0].description;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+}
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "5779c89c46453cf0e47b584677d15c00";
+  let city = document.querySelector("#city-input").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+function searchLocation(position) {
+  let apiKey = "5779c89c46453cf0e47b584677d15c00";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showTemp);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentLocationButton = document.querySelector("#currentLocation");
+currentLocationButton.addEventListener("click", getCurrentLocation);
